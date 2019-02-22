8c31c21f9027d50720a2bad154b656b380d31b3e
OoiTengHong <34025654+OoiTengHong@users.noreply.github.com>
2019-02-22 13:26:03 +0800

    minor update for index

---
+++ 1.Automatic Film Processor.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Automatic Film Processor</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Footer-Clean.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
+    <link rel="stylesheet" type="text/css" href="owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="owl.theme.default.css">
+</head>
+<body>
+	<nav class="navbar navbar-light navbar-expand-md navigation-clean-button sticky-top" data-aos="fade-up" data-aos-duration="700">
+        <div class="container"><a class="navbar-brand" href="#" data-bs-hover-animate="bounce"><i class="fas fa-home"></i> Home</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+           <div class="collapse navbar-collapse" id="navcol-1">
+               <ul class="nav navbar-nav mr-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#equipments"> Equipment</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#accessories"> Accessories</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#supplies"> Supplies</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#parts"> Parts</a></li>
+                </ul>
+                <span class="navbar-text actions"> 
+                    <a href="1.Sign In.php" class="login">Log In</a>
+                    <a class="btn btn-light action-button" role="button" href="2.Sign Up.php" data-bs-hover-animate="pulse">Sign Up</a>
+                </span>
+            </div>              
+        </div>
+    </nav>
+
+	<h1>testing</h1>
+	<script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-animation.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+    <script src="assets/js/Simple-Slider1.js"></script>
+    <script type="text/javascript" src="owl.carousel.js"></script>
+</body>
+</html>
---
+++ Automatic Film Processor.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Automatic Film Processor</title>
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/css/Footer-Clean.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/css/swiper.min.css">
+    <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+    <link rel="stylesheet" href="assets/css/Simple-Slider.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/Team-Boxed.css">
+    <link rel="stylesheet" type="text/css" href="owl.carousel.css">
+    <link rel="stylesheet" type="text/css" href="owl.theme.default.css">
+    <style type="text/css">
+        .eq-button {
+            margin-left: 45px;
+            
+        }
+        .card-head {
+            height: 300px;
+        }
+        .card-body {
+            height: 230px;
+        }
+        .title {
+            margin-top: 20px;
+            height: 110px;
+            font-size: 20px;
+            font-weight: 400;
+        }
+    </style>
+</head>
+<body>
+	<nav class="navbar navbar-light navbar-expand-md navigation-clean-button sticky-top" data-aos="fade-up" data-aos-duration="700">
+        <div class="container"><a class="navbar-brand" href="#" data-bs-hover-animate="bounce"><i class="fas fa-home"></i> Home</a><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+           <div class="collapse navbar-collapse" id="navcol-1">
+               <ul class="nav navbar-nav mr-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#equipments"> Equipment</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#accessories"> Accessories</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#supplies"> Supplies</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="#parts"> Parts</a></li>
+                </ul>
+                <span class="navbar-text actions"> 
+                    <a href="1.Sign In.php" class="login">Log In</a>
+                    <a class="btn btn-light action-button" role="button" href="2.Sign Up.php" data-bs-hover-animate="pulse">Sign Up</a>
+                </span>
+            </div>              
+        </div>
+    </nav>
+
+	<div class="container">
+        <div class="row">
+             <div class="item col-md-3">
+                <div class="card">
+                        <div class="card-head">
+                            <img src="assets/img/Automatic Film Processor/01.jpg" alt="img" class="card-img-top">
+                        </div>
+                        <div class="card-body">
+                            
+                            <p class="text-center title">Automatic Film Processors</p>
+                            
+                            <button class="btn btn-primary btn-sm eq-button" onclick="window.location.href='Automatic Film Processor.php'">More Info</button>
+                        </div>
+                </div>    
+            </div>
+        </div>   
+    </div> 
+
+	<script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-animation.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+    <script src="assets/js/Simple-Slider1.js"></script>
+    <script type="text/javascript" src="owl.carousel.js"></script>
+</body>
+</html>
